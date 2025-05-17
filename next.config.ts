6fac54d55d719b02a9837ce8f1ffc235c1b31364
aditya-manohar <adityamanohar2004@gmail.com>
2025-05-17 15:26:17 +0530

    removed basepath and assetprefix in next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Optional: basePath must match your repo name
-  basePath: '/rising-capital',
-  // Optional: assetPrefix to ensure static assets work correctly
-  assetPrefix: '/rising-capital/',
 };
 
 export default nextConfig;
