c21a23dace1e31853ef2c8b36aeb518dacf9156f
sainsg333 <sainsg333@gmail.com>
2025-07-29 16:52:45 +0530

    deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isGitHubPages = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
-    images: { unoptimized: true },
-  basePath: isGitHubPages ? '/ui' : '',
-  assetPrefix: isGitHubPages ? '/ui/' : '',
+  images: { unoptimized: true },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
