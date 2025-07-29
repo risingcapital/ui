fc7d9c05ced4876b3ce5812d7a1b5dc7bbdd1e07
sainsg333 <sainsg333@gmail.com>
2025-07-29 16:27:01 +0530

    Clean rebuild and fresh deploy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isGitHubPages = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
+    images: { unoptimized: true },
+  basePath: isGitHubPages ? '/ui' : '',
+  assetPrefix: isGitHubPages ? '/ui/' : '',
 };
 
 export default nextConfig;
